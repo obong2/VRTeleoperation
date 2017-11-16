cec10a6f80c1f214d35d7920d68ee41f5bac5fb8
Yeonju Oh <oh132@purdue.edu>
2017-11-16 01:12:01 -0500

    Unity temp commit

---
+++ Unity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public Dropdown dropdown0;
+    public Dropdown dropdown1;
+    public Dropdown dropdown2;
+
+    List<string> APs = new List<string>() { "AP1", "AP2", "AP3" };
+    public Text selectedItem;
+
+    // Use this for initialization
+    void Start () {
+        PopulateList();
+	}
+	
+    void PopulateList()
+    {   
+        dropdown0.AddOptions(APs);
+        dropdown1.AddOptions(APs);
+        dropdown2.AddOptions(APs);
+    }
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
